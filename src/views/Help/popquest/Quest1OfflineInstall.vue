721a16d9b6329a638699d972569b9dacdeca851b
orchid2ev <bas-team@gmail.com>
2020-04-14 16:34:40 +0800

    Apply Search Tabs background image

--- src/views/Help/popquest/Quest1OfflineInstall.vue
+++ src/views/Help/popquest/Quest1OfflineInstall.vue
@@ -1,46 +1,38 @@
 <template>
   <div>
     <h6 class="text-center pb-3 pt-2">
-      {{$t('p.HelpPopQuest1InnerTitle')}}
+      <!-- {{$t('p.HelpPopQuest1InnerTitle')}} -->
     </h6>
     <div class="bas-issue-section">
+      <h6>{{$t('p.HelpQuest1Step1Title')}}</h6>
       <p>
-        {{$t('p.HelpPopQuest1InnerP01')}}
-      </p>
-    </div>
-    <div class="bas-issue-section">
-      <h6>步骤一、下载安装文件</h6>
-      <p>
-        点击下方按钮下载 zip 文件，等待下载完成后，将文件解压缩。
+        {{$t('p.HelpQuest1DownloadBtnPrefix')}}
       </p>
       <el-button @click="downloadExtHandle" class="bas-btn-primary">
-        Download Chrome extensions
+        Download Extensions
       </el-button>
     </div>
     <div class="bas-issue-section">
-      <h6>步骤二、打开“扩展程序”页面</h6>
+      <h6>{{$t('p.HelpQuest1Step2Title')}}</h6>
       <p>
-      点击 Chrome 浏览器右上角菜单栏，从“更多工具”中打开“扩展程序”。
+        {{$t('p.HelpQuest1Step2Desc1')}}
       </p>
       <img :src="isCN ? 'static/img/help/quest_basext_01.png' : 'static/img/help/quest_basext_01_en.png'"
         class="img-fluid">
     </div>
     <div class="bas-issue-section">
-      <h6>步骤三、在右上角打开“开发者模式</h6>
+      <h6>{{$t('p.HelpQuest1Step3Title')}}</h6>
       <p>
-        点击下方按钮下载 zip 文件，等待下载完成后，将文件解压缩。在右上角找到开发者模式按钮，点击开启
+        {{$t('p.HelpQuest1Step3Desc1')}}
       </p>
       <img src="static/img/help/quest_basext_02.png" class="img-fluid">
     </div>
     <div class="bas-issue-section">
-      <h6>步骤四、安装扩展程序</h6>
+      <h6>{{$t('p.HelpQuest1Step4Title')}}</h6>
       <p>
-        在右上角找到开发者模式按钮，点击开启
+        {{$t('p.HelpQuest1Step4Desc1')}}
       </p>
-      <img src="static/img/help/quest_basext_03.png" class="img-fluid">
-      <p>将下载的BAS插件.zip直接拖入Chrome插件管理界面安装.</p>
       <img :src="isCN ? 'static/img/help/chrome_drop_install.png':'static/img/help/chrome_drop_install_en.png'" class="img-fluid">
-      <p>也可以解压(推荐用winrar)插件后,点击左侧“加载以解压的扩展程序”，选择刚才解压缩后的文件夹，点击“选择”，完成安装</p>
       <img src="static/img/help/quest_basext_04.png" class="img-fluid">
     </div>
   </div>

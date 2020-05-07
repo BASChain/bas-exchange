6b5e3f68ec18b062944e81225f05bbb719d82520
orchid2ev <bas-team@gmail.com>
2020-05-07 23:50:56 +0800

    update address manager

---
+++ src/web3-lib/abi-manager/address-holder.js
@@ -0,0 +1,113 @@
+import ContractNames from './name-enums'
+
+const ContractAddresses = {
+  [ContractNames.BasAccountant]: {
+    1: "",
+    3: "0xeF4Ec28594991f385a79fEBfB71EB18cc7da272A"
+  },
+  [ContractNames.BasDomainConf]: {
+    1: "",
+    3: "0x95F733f7D86944bB4adC8E258296A312F7D7F13E"
+  },
+  [ContractNames.BasExpireOwnership]: {
+    1: "",
+    3: "0xEB5a765d7e7802C3CDA01372eC6827E8fbab04B9"
+  },
+  [ContractNames.BasMail]: {
+    1: "",
+    3: "0x6b83970a7ae5a51A2CF6f94485dB0d537629736C"
+  },
+  [ContractNames.BasMailManager]: {
+    1: "",
+    3: "0xDa9d54E2a517AEe4339832bfE7a74d87513AeD9d"
+  },
+  [ContractNames.BasMarket]: {
+    1: "",
+    3: "0x7c31C4Ed7e30082B913ece060FF12DE77DE7d260"
+  },
+  [ContractNames.BasMiner]: {
+    1: "",
+    3: "0xfc9fecfbDB278FFd879ea1205a2e7503E1fd2bfC"
+  },
+  [ContractNames.BasOANN]: {
+    1: "",
+    3: "0xe124EA51E453e14F4D3f131bd28345382Ff39b37"
+  },
+  [ContractNames.BasRootDomain]: {
+    1: "",
+    3: "0xA64732B796255c7065e2bD5CC32e9898e9EdA752"
+  },
+  [ContractNames.BasSubDomain]: {
+    1: "",
+    3: "0x0fA0408ed2897Ee0c07a3A3Bbd60b83093C55995"
+  },
+  [ContractNames.BasToken]: {
+    1: "",
+    3: "0xC51A675D2f18e9912E3bd67014F0892177A9318C"
+  },
+  [ContractNames.BasTradableOwnership]: {
+    1: "",
+    3: "0xCec172c9a97B26C78BA00704A8598Bbbbc14e75c"
+  },
+  [ContractNames.BasView]: {
+    1: "",
+    3: "0xF696D463bb874cC44Db99Ae16BAaba3d16127e1C"
+  }
+};
+
+const LocalAddresses = {
+  [ContractNames.BasAccountant]: {
+    1: "",
+    3: "0x95167032CFfa2edD354Ba109B3D8867B0aA914ea"
+  },
+  [ContractNames.BasDomainConf]: {
+    1: "",
+    3: "0x72cC5096f22ef881188a42FD32f96e2443821dF8"
+  },
+  [ContractNames.BasExpireOwnership]: {
+    1: "",
+    3: "0xF5E47f03955f0be82C5C77E233063160572320fF"
+  },
+  [ContractNames.BasMail]: {
+    1: "",
+    3: "0xEe2e88927dC75c7E25406456613Eac3Be24d4661"
+  },
+  [ContractNames.BasMailManager]: {
+    1: "",
+    3: "0xf7fC9d1e9c293dC0D8eCeF199f4e62BF1e755233"
+  },
+  [ContractNames.BasMarket]: {
+    1: "",
+    3: "0x98c6f145B8fE9e77e3bBeA47165c3c39beeb6FE6"
+  },
+  [ContractNames.BasMiner]: {
+    1: "",
+    3: "0x720Da26432b6501AeD0020dA04ee67257928d9BD"
+  },
+  [ContractNames.BasOANN]: {
+    1: "",
+    3: "0x948aaD089241c7D8E04F030b071ABa3DC49c30e3"
+  },
+  [ContractNames.BasRootDomain]: {
+    1: "",
+    3: "0x1d543e75fE6CF7ce3e61da92A25EF3984eBe352E"
+  },
+  [ContractNames.BasSubDomain]: {
+    1: "",
+    3: "0xa3F8a61fA43Af82CAfd5C98d30A4Af7AC8357fbe"
+  },
+  [ContractNames.BasToken]: {
+    1: "",
+    3: "0xca2140437D9748Af1DA883cADD5997C56A1ff19D"
+  },
+  [ContractNames.BasTradableOwnership]: {
+    1: "",
+    3: "0x89123fD3EeF978b9656cB9A8174b6DDbe39E8b27"
+  },
+  [ContractNames.BasView]: {
+    1: "",
+    3: "0xf74b2b8E7a6A75222157a26a65554C1587fACED9"
+  }
+};
+
+export default ContractAddresses;

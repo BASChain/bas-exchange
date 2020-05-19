50775b8b189c993450e5c5bc61b65b8d29dd639b
orchid2ev <bas-team@gmail.com>
2020-05-19 10:09:37 +0800

    fixed milestone line

---
+++ src/views/Mail/MailRegisting.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name:"MailRegisting",
+  components:{
+
+  },
+  computed: {
+
+  },
+  data() {
+    return {
+      domaintext:'',
+      domainHash:'',
+      mailtext:'',
+      years:1,
+      mailHash:'',
+      txHashes:[]
+    }
+  },
+  mounted() {
+
+  },
+}
+</script>
+<style>
+
+</style>
